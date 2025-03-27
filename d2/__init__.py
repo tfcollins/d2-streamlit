fb3076760dadcba29f0fa275cc235e7aa81e8cef
Travis F. Collins <travis.collins@analog.com>
2025-03-26 21:03:43 -0600

    Change CI build directory
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- d2/__init__.py
+++ d2/__init__.py
@@ -5,9 +5,9 @@ import streamlit.components.v1 as components
 # the component, and True when we're ready to package and distribute it.
 # (This is, of course, optional - there are innumerable ways to manage your
 # release process.)
-_RELEASE = False
-if os.environ.get("RELEASE", "false").lower() == "true":
-    _RELEASE = True
+_RELEASE = True
+if os.environ.get("DEV", "false").lower() == "true":
+    _RELEASE = False
 
 # Declare a Streamlit component. `declare_component` returns a function
 # that is used to create instances of the component. We're naming this
